9b906d6cd2108fba2c45a0b27b66282033a74965
Appian <zhang4baichi@sina.com>
2017-10-30 17:49:01 +0800

    test post 请求2

--- server/router/api.router.ts
+++ server/router/api.router.ts
@@ -1,13 +1,22 @@
 import { Router, Request, Response, NextFunction } from 'express';
 const router = Router();
 // import checkToken from '../middleware/check.token';
-import TestApi from '../apis/test';
-import * as util from '../libs/util';
+import { get, post } from '../libs/axios';
 
 router.post('/auth', async function (req: Request, res: Response, next: NextFunction) {
     try {
-        console.log('**** this is state ****' + 123456789);
-        res.json(123456789);
+        post(req, 'http://54.222.196.128:8081/api/v1/uac/oauth/token', {
+            headers: {
+                Authorization: 'Basic c29wX2FwcF9wbGF0Zm9ybTpZWEJ3Y0d4aGRHWnZjbTFmYzJWamNtVjA='
+            },
+            data: {
+                grant_type: 'password',
+                username : '15160039391',
+                password: '151600393912017'
+            }
+        }).then(data => {
+            res.send(data);
+        })
     } catch (err) {
         next(err);
     }
