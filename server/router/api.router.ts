3673a5adc411759d7c09d43e61acaa20b509eb6d
Appian <zhang4baichi@sina.com>
2017-10-31 17:18:07 +0800

    test post 请求7

--- server/router/api.router.ts
+++ server/router/api.router.ts
@@ -2,13 +2,15 @@ import { Router, Request, Response, NextFunction } from 'express';
 const router = Router();
 // import checkToken from '../middleware/check.token';
 import { get, post } from '../libs/axios';
-import Axios from 'axios';
 
 router.post('/auth', async function (req: Request, res: Response, next: NextFunction) {
     try {
         console.log('--- axios开始请求值 ---')
-        Axios.get('https://api.github.com/search/users?q=kodo')
-            .then(function(data) {
+        get(req, 'https://api.github.com/search/users', {
+            data : {
+                q: 'appian'
+            }
+        }).then(function(data) {
                 console.log('--- axios结束值 ---')
                 console.log(JSON.stringify(data.data.items));
                 res.send(JSON.stringify(data.data.items));
