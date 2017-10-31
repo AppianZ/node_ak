c49e09f1c2118b6b78f47abd4a797dcd0cf7db06
Appian <zhang4baichi@sina.com>
2017-10-31 17:28:55 +0800

    test post 请求9

--- server/router/api.router.ts
+++ server/router/api.router.ts
@@ -8,15 +8,15 @@ router.post('/auth', async function (req: Request, res: Response, next: NextFunc
         console.log('--- axios开始请求值 ---')
         Axios({
             method:'post',
-            baseURL: 'http://54.222.196.128:8081/api',
+            baseURL: 'http://54.222.196.128:8081/api/',
             url:'/v1/uac/oauth/token',
             headers: {'Authorization': 'Basic c29wX2FwcF9wbGF0Zm9ybTpZWEJ3Y0d4aGRHWnZjbTFmYzJWamNtVjA=',
                 'Content-Type': 'application/x-www-form-urlencoded'},
-            data: {
+            data: JSON.stringify({
                 grant_type: 'password',
                 username: 15160039391,
                 password: 151600393912017
-            },
+            }),
         })
             .then(function(data) {
                 console.log('--- axios结束值 ---')
