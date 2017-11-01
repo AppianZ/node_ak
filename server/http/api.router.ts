06f717e7e4e04f29554d3b388841305ce9a6ba42
Appian <zhang4baichi@sina.com>
2017-11-01 16:34:29 +0800

    test axios create get3

--- server/http/api.router.ts
+++ server/http/api.router.ts
@@ -7,13 +7,7 @@ import * as http from '../libs/axios';
 router.post('/auth', async function (req: Request, res: Response, next: NextFunction) {
     try {
         console.log('--- axios开始请求值 ---')
-
-        let res = await http.get(req, `search/users`, {
-            data: {
-                "q": "appian",
-            },
-        });
-        console.log(res);
+        console.log(await http.get(req, 'search/users?q=appian'));
         console.log('--- axios 结束值 ---')
     } catch (err) {
         next(err);
