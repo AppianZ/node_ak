96d5f8a910f2f65c82317c3795ba1b6e6e631e45
Appian <zhang4baichi@sina.com>
2017-11-02 09:29:37 +0800

    axios init 2

--- server/http/api.router.ts
+++ server/http/api.router.ts
@@ -4,19 +4,15 @@ const router = Router();
 import Axios from 'axios';
 
 router.post('/auth', async function (req: Request, res: Response, next: NextFunction) {
-    try {
-        console.log('--- axios开始请求值 ---')
-        Axios.get('https://api.github.com/search/users?q=appian')
-            .then((res) => {
-                console.log('--- axios 请求结束 ---')
-                console.log(res.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    } catch (err) {
-        next(err);
-    }
+    console.log('--- axios开始请求值 ---')
+    Axios.get('https://api.github.com/search/users?q=appian')
+        .then((res) => {
+            console.log('--- axios 请求结束 ---')
+            console.log(res.data)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 });
 
 module.exports = function (app) {
