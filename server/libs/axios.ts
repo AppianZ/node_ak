4c29551ecbf67a0ac0b771cb5ab768827714216b
Appian <zhang4baichi@sina.com>
2017-11-02 11:55:16 +0800

    test axios url generator2

--- server/libs/axios.ts
+++ server/libs/axios.ts
@@ -30,7 +30,7 @@ function generatorUrl(url: string, data: any = {}) {
 function ajax(req: Request, options: any = {}) {
     const method = options.method || 'get';
     const data = options.data || {};
-    const url = options.method == 'get' ? generatorUrl(options.url, options.data) : options.url;
+    const url = method == 'get' ? generatorUrl(options.url, data) : options.url;
     /*   const headers = Object.assign({
      'x-auth-token': req['x-auth-token'] || '',
      }, options.headers || {});*/
