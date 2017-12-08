08977852e0fe56d9848f20f96fac8cb8822acf65
Appian <zhang4baichi@sina.com>
2017-12-08 16:12:54 +0800

    test websocket fail

--- server/bin/www.ts
+++ server/bin/www.ts
@@ -47,9 +47,35 @@ var targetSocketArray = [];
 var roomGroupList = [];
 
 io.on('connection', function (socket) {
-
     console.log('in connection callback--  ' , socket);
 
+    socket.on('joinToRoom', function (data) {
+        socket.join(data.roomGroupId)
+        console.log('--- joinToRoom ---- ' + data.roomGroupId);
+        roomGroupList.push(data.roomGroupId);
+    })
+
+    socket.on('addUser', function (data, func) {
+        targetSocketArray.push(data.user);
+        console.log('--- addUser ---- ' + targetSocketArray);
+        socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+            return item.roomGroupId == data.roomGroupId;
+        }));
+        func(targetSocketArray);
+    });
+
+    socket.on('increaseCount', function (data) {
+        targetSocketArray.map(function(item) {
+            if(item.id == data.id) {
+                item.content = Object.assign({}, item.content, data.content);
+            }
+            return item;
+        })
+        console.log('--- increaseCount ---- ' + targetSocketArray);
+        socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+            return item.roomGroupId == data.roomGroupId;
+        }));
+    });
 
 });
 
