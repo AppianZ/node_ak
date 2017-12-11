7a7d09afe8898b2be4b4e20372c2199e48fe0c7a
Appian <zhang4baichi@sina.com>
2017-12-11 17:25:49 +0800

    add socketio into node14

--- server/router/test.router.ts
+++ server/router/test.router.ts
@@ -18,41 +18,40 @@ router.get('/', async function (req: Request, res: Response, next: NextFunction)
        var targetSocketArray = [];
         var roomGroupList = [];
 
-        // res.io.on('connection', function (socket) {
-        //     console.log('in connection callback--  ' , socket);
-        //
-        //     socket.on('joinToRoom', function (data) {
-        //         socket.join(data.roomGroupId)
-        //         console.log('--- joinToRoom ---- ' + data.roomGroupId);
-        //         roomGroupList.push(data.roomGroupId);
-        //     })
-        //
-        //     socket.on('addUser', function (data, func) {
-        //         targetSocketArray.push(data.user);
-        //         console.log('--- addUser ---- ' + targetSocketArray);
-        //         socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
-        //             return item.roomGroupId == data.roomGroupId;
-        //         }));
-        //         func(targetSocketArray);
-        //     });
-        //
-        //     socket.on('increaseCount', function (data) {
-        //         targetSocketArray.map(function(item) {
-        //             if(item.id == data.id) {
-        //                 item.content = Object.assign({}, item.content, data.content);
-        //             }
-        //             return item;
-        //         })
-        //         console.log('--- increaseCount ---- ' + targetSocketArray);
-        //         socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
-        //             return item.roomGroupId == data.roomGroupId;
-        //         }));
-        //     });
-        //
-        // });
+        res.io.on('connection', function (socket) {
+            console.log('in connection callback--  ' , socket);
+
+            socket.on('joinToRoom', function (data) {
+                socket.join(data.roomGroupId)
+                console.log('--- joinToRoom ---- ' + data.roomGroupId);
+                roomGroupList.push(data.roomGroupId);
+            })
+
+            socket.on('addUser', function (data, func) {
+                targetSocketArray.push(data.user);
+                console.log('--- addUser ---- ' + targetSocketArray);
+                socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+                    return item.roomGroupId == data.roomGroupId;
+                }));
+                func(targetSocketArray);
+            });
+
+            socket.on('increaseCount', function (data) {
+                targetSocketArray.map(function(item) {
+                    if(item.id == data.id) {
+                        item.content = Object.assign({}, item.content, data.content);
+                    }
+                    return item;
+                })
+                console.log('--- increaseCount ---- ' + targetSocketArray);
+                socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+                    return item.roomGroupId == data.roomGroupId;
+                }));
+            });
+
+         });
 
         res.baseRender('test/index', state);
-        res.io.emit("showUser", "users123");
     } catch (err) {
         next(err);
     }
