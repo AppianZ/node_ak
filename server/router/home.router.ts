6b3f5045dca8381fa5051f6fe171d65f4590031b
Appian <zhang4baichi@sina.com>
2017-12-12 10:46:44 +0800

    add home router

---
+++ server/router/home.router.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response, NextFunction } from 'express';
+const router = Router();
+// import checkToken from '../middleware/check.token';
+import TestApi from '../apis/test';
+import * as util from '../libs/util';
+
+router.get('/', async function (req: Request, res: Response, next: NextFunction) {
+    const user = req.query.user || 'testuser1';
+
+    const $state = new TestApi(req).getTestInit(user);
+    try {
+        const state:any = await $state;
+        res.baseRender('home/home', state);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+router.get('/test', async function (req: Request, res: Response, next: NextFunction) {
+
+    try {
+        res.baseRender('test/test', {
+            msg: 'okkkkk'
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = function (app) {
+  app.use('/home', router);
+};
