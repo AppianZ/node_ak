482d1b3a1efd29c462338d69d117ec80f0090d3e
Appian <zhang4baichi@sina.com>
2017-12-15 17:26:57 +0800

    测试倒计时后的websocket 8

--- server/app.ts
+++ server/app.ts
@@ -30,6 +30,69 @@ httpFiles.forEach((file) => {
 });
 
 
+
+/**
+ * websocket work.
+ */
+var targetSocketArray = [];
+var roomGroupList = [];
+var allClients = [];
+
+io.on('connection', function (socket) {
+    allClients.push(socket);
+    console.log('~~ connection' + socket.id);
+    socket.on('disconnect', function (data) {
+        socket.emit('disconnected');
+        console.log(data);
+        var i = allClients.indexOf(socket);
+        allClients.splice(i, 1);
+    });
+
+    socket.on('joinToRoom', function (data) {
+        socket.join(data.roomGroupId)
+        if(roomGroupList.indexOf(data.roomGroupId < 0)) {
+            roomGroupList.push(data.roomGroupId);
+        }
+    })
+
+    socket.on('addUser', function (data, func) {
+        targetSocketArray.push(data.user);
+        socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+            return item.roomGroupId == data.roomGroupId;
+        }));
+        func(targetSocketArray.filter(function (item) {
+            return item.roomGroupId == data.roomGroupId;
+        }));
+    });
+
+    socket.on('onTimeCount', function (data, func) {
+        console.log ('aaaaa--- ' + data.isStart);
+        socket.in(data.roomGroupId).emit('timeDecrease', {
+            isEnd: data.isStart == 2,
+            isWait: data.isStart == 0,
+            time: data.time
+        });
+
+        func({
+            time : data.time,
+            isStart: data.isStart,
+        });
+    });
+
+    socket.on('increaseCount', function (data) {
+        targetSocketArray.map(function(item) {
+            if(item.id == data.id) {
+                item.content = Object.assign({}, item.content, data.content);
+            }
+            return item;
+        })
+        socket.in(data.roomGroupId).emit('showUser', targetSocketArray.filter(function (item) {
+            return item.roomGroupId == data.roomGroupId;
+        }));
+    });
+
+});
+
 app.use(async(err, req: Request, res: Response, next: NextFunction) => {
   if (!err.response) { // node 挂了
     err = new Error(err);
